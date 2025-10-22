86949517f3081c3e0ecd62358c60e9f2554dbe54
Dursun <dursunpekcan7@gmail.com>
2025-10-22 22:02:50 +0300

    project created and variables created

---
+++ main.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    double sayi1, sayi2;
+    int secim;
+
+    // Kullanıcıdan sayıları alma
+    cout << "Birinci sayıyı girin: ";
+    cin >> sayi1;
+    cout << "İkinci sayıyı girin: ";
+    cin >> sayi2;
+
+    // Menü gösterme
+    cout << "\nYapmak istediğiniz işlemi seçin:\n";
+    cout << "1. Toplama\n";
+    cout << "2. Çıkarma\n";
+    cout << "3. Çarpma\n";
+    cout << "4. Bölme\n";
+    cout << "Seçiminiz: ";
+    cin >> secim;
+
+    
+    
+
+    return 0;
+}
